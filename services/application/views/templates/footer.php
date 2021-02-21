b40ebc8a9999cac45df1429bce0686001146944f
Daniel Alberto Rosso <danielalbertorosso@hotmail.com>
2021-02-21 16:06:05 -0300

    Update footer dashboard

--- services/application/views/templates/footer.php
+++ services/application/views/templates/footer.php
@@ -2,8 +2,18 @@
         <!-- /page content -->
         
         <!-- Footer -->                    
-        <footer class="footer clearfix" id="footerTSoft">
+        <footer class="footer clearfix" id="footerTSoft" style="background: #DADFED;">
             <div class="text-center">
+                <a href="#" target="_blank" style="text-decoration:none;">
+                    <img id="logoFooterCR" src="<?=$url?>assets/images/logoRed/redhot.png">
+                    <strong>Red Hot Chili Devs</strong> 
+                </a>
+                Copyright &#169; <?=$anioFooter?> </a>
+                <a href="#" class="go-top">
+                   <i class="icon-angle-up"></i>
+                </a>
+            </div>
+            <!-- <div class="text-center">
                 <a href="http://empresa.com.ar" target="_blank" style="text-decoration:none;">
                     <img id="logoFooterCR" src="<?=$url?>">
                     <strong>Empresa</strong> 
@@ -12,7 +22,7 @@
                 <a href="#" class="go-top">
                    <i class="icon-angle-up"></i>
                 </a>
-            </div>
+            </div> -->
         </footer>                        
         <!-- /footer -->        
         
